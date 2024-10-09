9df9387a0399223dac247d61e51981c5989e1190
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2024-10-09 08:21:26 -0500

    endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const app = express(); // Creates HTTP server
 app.use(express.json()); // utility to process JSON in requests
@@ -10,43 +10,97 @@ app.use(cors()); // utility to allow clients to make requests from other hosts o
 const httpServer = createServer(app); // Explicity creates an HTTP server from the Express app
 
 const io = new Server(httpServer, {
-  path: "/real-time",
-  cors: {
-    origin: "*", // Allow requests from any origin
-  },
+	path: '/real-time',
+	cors: {
+		origin: '*', // Allow requests from any origin
+	},
 }); // Creates a WebSocket server, using the same HTTP server as the Express app and listening on the /real-time path
 
 const db = {
-  players: [],
+	players: [],
 };
 
-app.get("/hola", (request, response) => {
-  response.send(db);
+app.get('/userTime', (request, response) => {
+	response.send(db);
 });
 
-app.post("/potenciometro", (request, response) => {
-  const { body } = request;
-  console.log("potValue", body);
-  response.status(201).send(body); // We return the same object received and also I send a code 201 which means an object was created
-  //io.emit("potValue", body.value); // I can emit events to all connected clients from a single endpoint
+app.post('/onSelectedAnimal', (request, response) => {
+	response.status(201).send(body); // We return the same object received and also I send a code 201 which means an object was created
 });
 
-app.post("/button", (request, response) => {
-  const { body } = request;
-  console.log("button", body);
-  response.status(201).send(body); // We return the same object received and also I send a code 201 which means an object was created
+app.post('/onConfirmation', (request, response) => {
+	response.status(201).send(body);
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected"); // This will be printed every time a client connects to the
-  socket.on("message", (message) => {
-    console.log(message);
-    io.emit("chat-messages", message); // Broadcasts the message to all connected clients including the sender
-    // socket.broadcast.emit("chat-messages", message); // Broadcasts the message to all connected clients except the sender
-  });
+app.post('/onUserData', (request, response) => {
+	response.status(201).send(body);
+});
+
+app.post('/crossedFirstLine', (request, response) => {
+	response.status(201).send(body);
+});
+
+app.post('/crossedSecondLine', (request, response) => {
+	response.status(201).send(body);
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected'); // This will be printed every time a client connects to the
+
+	socket.on('userConnected', (userConnected) => {
+		io.emit('userConnected', userConnected);
+	});
+
+	socket.on('selectedAnimal', (selectedAnimal) => {
+		io.emit('selectedAnimal', selectedAnimal);
+	});
+
+	socket.on('confirmation', (confirmation) => {
+		io.emit('confirmation', confirmation);
+	});
+
+	socket.on('userRegistered', (userRegistered) => {
+		io.emit('userRegistered', userRegistered);
+	});
+
+	socket.on('selectedAnimal', (selectedAnimal) => {
+		io.emit('selectedAnimal', selectedAnimal);
+	});
+
+	socket.on('confirmation', (confirmation) => {
+		io.emit('confirmation', confirmation);
+	});
+
+	socket.on('userRegistered', (userRegistered) => {
+		io.emit('userRegistered', userRegistered);
+	});
+
+	socket.on('confirmation', (confirmation) => {
+		io.emit('confirmation', confirmation);
+	});
+
+	socket.on('userCrossedFirstLine', (userCrossedFirstLine) => {
+		io.emit('userCrossedFirstLine', userCrossedFirstLine);
+	});
+
+	socket.on('userCrossedSecondLine', (userCrossedSecondLine) => {
+		io.emit('userCrossedSecondLine', userCrossedSecondLine);
+	});
+
+	socket.on('userTime', (userTime) => {
+		io.emit('userTime', userTime);
+	});
+
+	socket.on('userWins', (userWins) => {
+		io.emit('userWins', userWins);
+	});
+
+	socket.on('animalWins', (animalWins) => {
+		io.emit('animalWins', animalWins);
+	});
 });
 
 httpServer.listen(5050, () => {
-  // Starts the server on port 5050, same as before but now we are using the httpServer object
-  console.log(`Server is running on http://localhost:${5050}`);
+	// Starts the server on port 5050, same as before but now we are using the httpServer object
+	console.log(`Server is running on http://localhost:${5050}`);
 });
