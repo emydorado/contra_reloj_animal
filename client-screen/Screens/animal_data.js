aff45258ed381a32505750de91c6110f75f447a3
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2024-11-26 11:41:32 -0500

    css animal data

--- client-screen/Screens/animal_data.js
+++ client-screen/Screens/animal_data.js
@@ -7,10 +7,31 @@ export default function renderAnimalData(id) {
 	const app = document.getElementById('app');
 	if (animalData) {
 		app.innerHTML = `
-			<h1>${animalData.nombre} Data</h1>
-			<p>${animalData.habitat}</p>
-			<p>${animalData.nombre} time for this race is ${animalData.segundos} seconds</p>
-		`;
+			<div id="topImageDiv">
+  		<img id="topImage" src="./resources/contraReloj.png" alt="Imagen header">
+			</div>
+
+			<div id="img_info_title">
+			<h1>Conoce más sobre el ${animalData.nombre}</h1>
+			<div id="img_info">
+			<div id="image">
+			<img ${animalData.img}>
+			</div>
+
+			<div id="info">
+			<p><b>Nombre científico: </b>${animalData.nombreCientifico}</p>
+			<p><b>Familia: </b>${animalData.familia}</p>
+			<p><b>Habitat: </b>${animalData.habitat}</p>
+			<p><b>Altura: </b>${animalData.altura}</p>
+			<p><b>Peso: </b>${animalData.peso}</p>
+			<p><b>Alimentación: </b>${animalData.alimentacion}</p>
+			<p><b>Velocidad: </b>${animalData.valocidad}</p>
+			<h3><b>Tiempo para esta carrera: </b>${animalData.segundos}s</h3>
+			</div>
+			</div>
+			<p>¡Este es el tiempo en el que el ${animalData.nombre} se tarda en dar 3 vueltas!</p>
+			</div>
+			`;
 	} else {
 		app.innerHTML = `
 			<h1>Animal Data</h1>
