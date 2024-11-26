2940db6a4724f4ad23531f93b24d4e7de53d0d7f
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2024-11-26 16:35:14 -0500

    css screen terminado

--- client-screen/Screens/loser.js
+++ client-screen/Screens/loser.js
@@ -3,23 +3,48 @@ import { router, socket } from '../routes.js';
 export default function renderLoser() {
 	const app = document.getElementById('app');
 	app.innerHTML = `
-        <h1>Animal Wins!</h1>
-				<p id="result">Animal wins!</p>
-				<p id="times">Waiting for times...</p>
+				<img id="topWinner" src="./resources/topWinner.png" alt="Imagen top">
+
+				<div id="timeContent">
+        <h1 id="ganador">¡Ha ganado el animal!</h1>
+
+        <div id="result-container">
+
+				<p><b>El tiempo del animal fue de</b></p>
+            <p class="result" id="animal-time"><span>Waiting...</span></p>
+
+				<p id="textTime"><b>Tu tiempo fue de</b></p>
+            <p class="result" id="user-time"><span>Waiting...</span></p>
+        </div>
+				</div>
+
+				<img id="pajaroTucan" src="./resources/pajaro-tucan.png" alt="Imagen middle">
+
+				<div class="reclamarPremio">
+				<h2 id="reclamarPremio">Presiona <b>¨Reclamar premio¨</b></h2>
+				<h2>en tu dispositivo</h2>
+				</div>
+
+				<div class="bottom">
+				<h3>¡Te esperamos!</h3>
+
+				<img id="logoZoo" src="./resources/logo-zoo.png" alt="logo">
+				</div>
+
     `;
 
 	const userTime = localStorage.getItem('userTime');
 	const animalTime = localStorage.getItem('animalTime');
 
 	if (userTime && animalTime) {
-		document.getElementById('times').innerText = `User Time: ${userTime} seconds, Animal Time: ${animalTime} seconds`;
+		document.getElementById('animal-time').querySelector('span').innerText = `${animalTime} segundos`;
+		document.getElementById('user-time').querySelector('span').innerText = `¡${userTime} segundos!`;
 	} else {
-		document.getElementById('times').innerText = 'Times not available.';
+		document.getElementById('animal-time').querySelector('span').innerText = 'No disponible.';
+		document.getElementById('user-time').querySelector('span').innerText = 'No disponible.';
 	}
 
 	socket.on('sendedPrize', (data) => {
-		console.log('hola');
-
 		router.navigateTo('/');
 	});
 }
--- client-screen/Screens/winner.js
+++ client-screen/Screens/winner.js
@@ -3,8 +3,33 @@ import { router, socket } from '../routes.js';
 export default function renderWinner() {
 	const app = document.getElementById('app');
 	app.innerHTML = `
-        <h1>You win!</h1>
-				<p id="times">Waiting for times...</p>
+					<img id="topWinner" src="./resources/topWinner.png" alt="Imagen top">
+
+				<div id="timeContent">
+        <h1 id="ganador">¡Ha ganado el animal!</h1>
+
+        <div id="result-container">
+
+				<p><b>El tiempo del animal fue de</b></p>
+            <p class="result" id="animal-time"><span>Waiting...</span></p>
+
+				<p id="textTime"><b>Tu tiempo fue de</b></p>
+            <p class="result" id="user-time"><span>Waiting...</span></p>
+        </div>
+				</div>
+
+				<img id="pajaroTucan" src="./resources/pajaro-tucan.png" alt="Imagen middle">
+
+				<div class="reclamarPremio">
+				<h2 id="reclamarPremio">Presiona <b>¨Reclamar premio¨</b></h2>
+				<h2>en tu dispositivo</h2>
+				</div>
+
+				<div class="bottom">
+				<h3>¡Te esperamos!</h3>
+
+				<img id="logoZoo" src="./resources/logo-zoo.png" alt="logo">
+				</div>
     `;
 
 	const userTime = localStorage.getItem('userTime');
@@ -14,9 +39,11 @@ export default function renderWinner() {
 	const animalId = selectedanimal.id;
 
 	if (userTime && animalTime) {
-		document.getElementById('times').innerText = `User Time: ${userTime} seconds, Animal Time: ${animalTime} seconds`;
+		document.getElementById('animal-time').querySelector('span').innerText = `${animalTime} segundos`;
+		document.getElementById('user-time').querySelector('span').innerText = `¡${userTime} segundos!`;
 	} else {
-		document.getElementById('times').innerText = 'Times not available.';
+		document.getElementById('animal-time').querySelector('span').innerText = 'No disponible.';
+		document.getElementById('user-time').querySelector('span').innerText = 'No disponible.';
 	}
 
 	socket.on('sendedPrize', (data) => {
