ab35fc1f5b52419f5f033751866f8135f2d9653b
25natalia <25natalia09garcia@gmail.com>
2024-11-25 19:11:31 -0500

    form listo

--- client-mobile/Screens/user_data.js
+++ client-mobile/Screens/user_data.js
@@ -3,13 +3,13 @@ import { router, socket } from '../routes.js';
 export default function renderUserData() {
 	const app = document.getElementById('app');
 	app.innerHTML = `
-        <h1>form user data</h1>
-				<p>Insert your data so we can send your prize</p>
-			    <input type="text" placeholder="Name" id="name"/>
-			    <input type="text" placeholder="Last Name" id="lastname"/>
-			    <input type="text" placeholder="Email" id="email"/>
-			    <button id="sendForm">Send form</button>
-    `;
+			<h1 id="title">Ingresa tus datos</h1>
+			<p id="Nombre">Insert your data so we can send your prize</p>
+			<input type="text" placeholder="Name" id="name"/>
+			<input type="text" placeholder="Last Name" id="lastname"/>
+			<input type="text" placeholder="Email" id="email"/>
+			<button id="sendForm">Send form</button>
+`;
 
 	document.getElementById('sendForm').addEventListener('click', () => {
 		const Name = document.getElementById('name').value;
